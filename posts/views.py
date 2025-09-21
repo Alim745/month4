59cd9e7488554e1416d1b61bbef1fb298f7ebb07
Alim Maratov <alimmaratov11@gmail.com>
2025-09-21 19:44:48 +0600

    hw2

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, HttpResponse
+from posts.models import Post
 
-# Create your views here.
 
 def test_view(request):
     return HttpResponse("First view is working")
 
 def html_view(request):
     return render(request, "base.html")
+
+def posts_list_view(request): 
+    posts = Post.objects.all()
+    return render(request, "posts/posts_list.html", context={"posts_list": posts})
+
+def post_detail_view(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, "posts/posts_list.html", context={"post": post})
